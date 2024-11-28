14783fc4f624ef59a525124956fe7f7bac11b104
lepc1972 <githublepc@gmail.com>
2024-11-28 16:15:13 -0500

    funciones

--- condicionales-bucles-mapas/bucles.go
+++ condicionales-bucles-mapas/bucles.go
@@ -6,9 +6,33 @@ func main() {
 
 	var suma int = 0
 
-	for i := 0; i <= 100; i++ {
-		suma = suma + i
+	for i := 0; i <= 10; i++ {
+		if i%2 != 0 {
+			suma = suma + i
+
+		}
+
 	}
 	fmt.Println(suma)
+	// especide de for each que recorre mapa
+	var miMapa = map[string]string{
+		"nombre":   "Juan",
+		"apellido": "Perez",
+		"edad":     "30",
+	}
+	for clave, valor := range miMapa {
+		fmt.Println(clave, valor)
+	}
+	//uso de while con for de go
+	var fruta string = ""
+
+	for {
+		if fruta == "manzana" {
+			break
+		}
+		fmt.Println("¿Qué fruta es?")
+		fmt.Scanln(&fruta)
+
+	}
 
 }
